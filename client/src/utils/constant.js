8bbc1056a3cd70a52dfebd1d3fc0540997a93f4b
Sona <sona2529001@gmail.com>
2026-06-07 18:00:37 +0530

    Fixed env file issue

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
+const API_BASE_URL = (import.meta.env.VITE_API_BASE_URL || "http://localhost:3000").replace(/\/$/, "");
 
 export const USER_API_END_POINT = `${API_BASE_URL}/api/v1/user`;
 export const JOB_API_END_POINT = `${API_BASE_URL}/api/v1/job`;
